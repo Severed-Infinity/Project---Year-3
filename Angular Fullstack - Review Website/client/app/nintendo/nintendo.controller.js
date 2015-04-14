120627d11a8a853840d80945578388e2a22b14be
Severed-Infinity <masterdavidswift@hotmail.com>
2015-04-14 21:49:19 +0100

    Created pages
    other game pages created
    signup and login fixed
    create review page only visible when logged in

--- Angular Fullstack - Review Website/client/app/nintendo/nintendo.controller.js
+++ Angular Fullstack - Review Website/client/app/nintendo/nintendo.controller.js
@@ -1,6 +1,32 @@
 'use strict';
 
 angular.module('reviewWebsiteApp')
-  .controller('NintendoCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('NintendoCtrl', function($scope, $http, socket) {
+      $scope.reviews = [];
+      //        {
+      //          title: 'test game',
+      //          publisher: 'test publisher'
+      //        },
+      //        {
+      //          title: 'test game 2',
+      //          publisher: 'test publisher 2'
+      //        },
+      //        {
+      //          title: 'test game 3',
+      //          publisher: 'test publisher 3'
+      //        }
+      //      ];
+
+      $http.get('/api/reviews').success(function(reviews) {
+        $scope.reviews = reviews;
+        socket.syncUpdates('review', $scope.reviews);
+      });
+
+      $scope.deleteReview = function(review) {
+        $http.delete('/api/reviews/' + review._id);
+      };
+
+      $scope.$on('$destroy', function() {
+        socket.unsyncUpdates('review');
+      });
+    });
