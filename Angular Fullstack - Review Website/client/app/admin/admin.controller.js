7bcc6a5b897ec7d6a45e278f05fcbe489b65daa6
Severed-Infinity <masterdavidswift@hotmail.com>
2015-04-15 11:25:06 +0100

    minor changes

--- Angular Fullstack - Review Website/client/app/admin/admin.controller.js
+++ Angular Fullstack - Review Website/client/app/admin/admin.controller.js
@@ -1,17 +1,39 @@
 'use strict';
 
 angular.module('reviewWebsiteApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function($scope, $http, Auth, User) {
 
-    // Use the User $resource to fetch all users
-    $scope.users = User.query();
+      // Use the User $resource to fetch all users
+      $scope.users = User.query();
 
-    $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
+      $scope.delete = function(user) {
+        User.remove({ id: user._id });
+        angular.forEach($scope.users, function(u, i) {
+          if (u === user) {
+            $scope.users.splice(i, 1);
+          }
+        });
+      };
+
+    }, function($scope, $http, Review) {
+
+      $scope.reviews = [];
+
+    $http.get('/api/reviews').success(function(reviews) {
+        $scope.reviews = reviews;
+        socket.syncUpdates('review', $scope.reviews);
       });
-    };
+
+      $scope.deleteReview = function(review) {
+        Review.remove({ id: review._id });
+        angular.forEach($scope.reviews, function(r, i) {
+          if (r === review) {
+            $scope.reviews.splice(i, 1);
+          }
+        });
+      };
+    }, function($scope, $http, Auth, Thing){
+      $scope.things = Thing.query();
+
+
   });
